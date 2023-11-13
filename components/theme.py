f369908cfc5ca100fa231e7189ed2afde29d4d49
hs_junxiang <jimmy871117@gmail.com>
2023-11-13 17:24:50 +0800

    change logo

--- components/theme.py
+++ components/theme.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 def theme() -> None:
     st.set_page_config(page_title="Document GPT")
-    st.image('./static/img/repos_logo.png', width=250)
+    st.image('./static/img/chatbot_v2.png', width=150)
